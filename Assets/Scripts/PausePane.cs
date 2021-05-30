218399d29f04ca3daea76f41301400e3072482ee
Code Fruit <61573357+1260839205@users.noreply.github.com>
2021-05-30 20:32:54 +0800

    Code Fruit

--- Assets/Scripts/PausePane.cs
+++ Assets/Scripts/PausePane.cs
@@ -12,13 +12,13 @@ public class PausePane : MonoBehaviour {
         anim = GetComponent<Animator>();
     }
 
+
     public void Retry()
     {
 
         Time.timeScale = 1;
         UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
-
     /// <summary>
     /// 点击了pause按钮
     /// </summary>
@@ -27,19 +27,54 @@ public class PausePane : MonoBehaviour {
         //1、播放pause动画  
         anim.SetBool("isPause", true);
         button.SetActive(false);
-
+        /*
         if (GameManager._instance.birds.Count > 0)
         {
             if (GameManager._instance.birds[0].isReleased == false)
             { //没有飞出
                 GameManager._instance.birds[0].canMove = false;
             }
-        }
+        }*/
+    }
+
+
+    /// <summary>
+    /// 点击了继续按钮
+    /// </summary>
+    public void Resume()
+    {
+        //1、播放resume动画
+        Time.timeScale = 1;
+        anim.SetBool("isPause", false);
+/*
+        if (GameManager._instance.birds.Count > 0)
+        {
+            if (GameManager._instance.birds[0].isReleased == false)
+            { //没有飞出
+                GameManager._instance.birds[0].canMove = true;
+            }
+        }*/
     }
 
     public void Home()
     {
         Time.timeScale = 1;
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+
+    /// <summary>
+    /// pause动画播放完调用
+    /// </summary>
+    public void PauseAnimEnd()
+    {
+        Time.timeScale = 0;
+    }
+    /// <summary>
+    /// resume动画播放完调用
+    /// </summary>
+    public void ResumeAnimEnd()
+    {
+        button.SetActive(true);
     }
 }
